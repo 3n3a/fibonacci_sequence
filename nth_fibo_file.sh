397c71d1436a73bebcdfbe30eec1f54526625ebd
3n3a <46775561+3n3a@users.noreply.github.com>
2020-12-18 14:16:57 +0100

    Update nth_fibo_file.sh

--- nth_fibo_file.sh
+++ nth_fibo_file.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-NUM=$(expr $1 + 1)
-sed "${NUM}q;d" fibonacci_1-999999999.txt
+HELP="file.sh <n-th term> <filename>"
+if [[ "$1" -eq "--help" ]]; then
+  echo $HELP
+elif [[ "$1" -eq "-h" ]]; then
+  echo $HELP
+elif [[ "$1" -gt 0 ]]; then
+  NUM=$(expr $1 + 1)
+  FILE=$2
+  sed "${NUM}q;d" $FILE 
+else
+  echo $HELP
+fi
