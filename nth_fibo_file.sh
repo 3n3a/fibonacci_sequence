c8ac4e8f09b9c57cd2ebdcce3a7280de84fa9fa3
3n3a <46775561+3n3a@users.noreply.github.com>
2020-12-18 14:19:58 +0100

    Update nth_fibo_file.sh

--- nth_fibo_file.sh
+++ nth_fibo_file.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-HELP="file.sh <n-th term> <filename>"
-if [[ "$1" -eq "--help" ]]; then
-  echo $HELP
-elif [[ "$1" -eq "-h" ]]; then
-  echo $HELP
-elif [[ "$1" -gt 0 ]]; then
-  NUM=$(expr $1 + 1)
-  FILE=$2
-  sed "${NUM}q;d" $FILE 
-else
-  echo $HELP
-fi
+NUM=$(expr $1 + 1)
+FILE=$2
+sed "${NUM}q;d" $FILE 
